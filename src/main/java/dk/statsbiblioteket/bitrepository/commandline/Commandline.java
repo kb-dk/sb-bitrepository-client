eb54a673be71ba10da542f850700e5ea87148db5
ktc <ktc@statsbiblioteket.dk>
2016-06-21 09:48:29 +0200

    add project setup, including script to start client

---
+++ src/main/java/dk/statsbiblioteket/bitrepository/commandline/Commandline.java
@@ -0,0 +1,8 @@
+package dk.statsbiblioteket.bitrepository.commandline;
+
+public class Commandline {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/scripts/sbclient.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$0")"
+JAVA_OPTS=" -Xmx256m"
+java $JAVA_OPTS -classpath "$SCRIPT_DIR/../lib/*" -Dlogback.configurationFile=$SCRIPT_DIR/../conf/logback.xml dk.statsbiblioteket.bitrepository.commandline.Commandline -c $SCRIPT_DIR/../conf/config.properties
+
